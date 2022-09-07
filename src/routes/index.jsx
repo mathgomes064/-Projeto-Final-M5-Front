b0cf5ac0a9da61033e84885149721c831ccb7183
Sidarta Kauã <odssy351@gmail.com>
2022-09-07 10:14:42 -0500

    feat: protectedRoutes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,14 +3,17 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import UserProfile from "../pages/UserProfile";
 import Dashboard from "../pages/Dashboard/dashboard.index";
+import ProtectedRoutes from "../components/ProtectedRoutes";
 
 const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/userprofile" element={<UserProfile />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+      </Route>
       <Route path="*" element={<Navigate to={"/"} replace />} />
     </Routes>
   );
