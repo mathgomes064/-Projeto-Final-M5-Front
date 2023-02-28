35cc1436e6c0d4790cc63d80a7e7d058701ab1fa
Matheus_Gomes <mathgomes007@htomail.com>
2023-02-27 23:53:18 -0300

    retorno para o desenvolvimento local

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nicejobs.onrender.com/api",
+  // baseURL: "https://nicejobs.onrender.com/api",
+  baseURL: "http://localhost:8000/api",
   timeout: 8000,
 });
 
